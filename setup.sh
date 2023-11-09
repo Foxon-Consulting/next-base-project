5ffd6c54741b4ca5a5f5a68dc23590ff2d01cd5f
fdsfds <fsdfs>
2023-11-09 21:51:05 +0000

    feat!: :tada: cookiecutter scaffold

--- setup.sh
+++ setup.sh
@@ -7,18 +7,18 @@ git config --global user.email $email
 git config --global user.name $name
 
 # Initialize the repository
-echo '''############################# Initializing repository ''#############################'
+echo '############################# Initializing repository #############################'
 git init
 git add .
 git commit -m "feat!: :tada: cookiecutter scaffold"
 
-echo '''############################# Installing pre-commit ''#############################'
+echo '############################# Installing pre-commit #############################'
 pre-commit install
 
 # install dependencies
-echo ''############################# Installing dependencies '#############################'
+echo '############################# Installing dependencies #############################'
 npm install
 
 # database initialization
-echo '############################# Prisma initialization '#############################'
+echo '############################# Prisma initialization #############################'
 npx prisma init --datasource-provider sqlite
