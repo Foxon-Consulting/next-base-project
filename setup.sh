14280b68bf0197cbcf6b3c838a4e95ad91e48bbf
myname <mymail@mail.com>
2023-11-09 20:46:34 +0000

    feat!: :tada: cookiecutter scaffold

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+read -p 'Enter user.email for git config: ' email
+read -p 'Enter user.name for git config: ' name
+
+git config --global user.email $email
+git config --global user.name $name
+
 # Initialize the repository
 git init
 git add .
@@ -10,5 +16,5 @@ pre-commit install
 # install dependencies
 npm install
 
-# database initialisation
+# database initialization
 npx prisma init --datasource-provider sqlite
