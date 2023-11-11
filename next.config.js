217cd33c19f27d8ac2299a4bea2f685221f16d0a
lizzieb1416 <lisetbastidasg@gmail.com>
2023-11-11 18:06:10 +0100

    feat: export static files config fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
+  output: "export", // before i had "standalone" instead of export. I don't know wyh. Maybe using this template in the future i will discover it.
 };
 
 module.exports = nextConfig;
