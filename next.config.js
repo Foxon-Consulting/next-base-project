93eb8910201fd99676a07b956d8f7096a001c1a8
lizzieb1416 <lisetbastidasg@gmail.com>
2024-02-16 22:52:30 +0100

    fix: output changed to export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone", // before i had "standalone" instead of export. I don't know wyh. Maybe using this template in the future i will discover it.
+  output: "export", // before i had "standalone" instead of export. I don't know wyh. Maybe using this template in the future i will discover it.
 };
 
 module.exports = nextConfig;
